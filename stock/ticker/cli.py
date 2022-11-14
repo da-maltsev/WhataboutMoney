77db7baede0ccb3e55b5c805241dab2bbc39ca64
da-maltsev <danil.zlatoust9999@gmail.com>
2022-11-14 17:18:11 +0500

    add Exception handle

--- stock/ticker/cli.py
+++ stock/ticker/cli.py
@@ -10,13 +10,19 @@ def cli():
 @click.command()
 @click.argument('ticker_name')
 def price(ticker_name):
-    click.echo(get_current_price(ticker_name))
+    try:
+        click.echo(get_current_price(ticker_name))
+    except Exception as e:
+        click.echo(e)
 
 
 @click.command()
 @click.argument('ticker_name')
 def stat(ticker_name):
-    click.echo(get_stat(ticker_name))
+    try:
+        click.echo(get_stat(ticker_name))
+    except Exception as e:
+        click.echo(e)
 
 
 cli.add_command(stat)

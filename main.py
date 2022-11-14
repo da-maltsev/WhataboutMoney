27a7a5424d54c57d493ef5f035fb712eeb5610d9
da-maltsev <danil.zlatoust9999@gmail.com>
2022-11-14 14:55:06 +0500

    tranform to Class

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
+import re
 import yfinance as yf
-import pprint
 
-pp = pprint.PrettyPrinter(indent=4)
 
-ticker_name = input()
-if len(ticker_name) > 4:
-    raise Exception('Incorrect ticker name')
+def input_ticker():
+    ticker_name = input().upper()
+    if not re.fullmatch(r'[A-Z]{2,4}', ticker_name):
+        raise Exception('Incorrect ticker name (should be less or equal to 4)')
+    return ticker_name
 
-ticker = yf.Ticker(ticker_name)
-if ticker.info['logo_url'] == '':
-    raise Exception('Such ticker does not exist')
 
-ticker_price = ticker.info['currentPrice']
-ticker_currency = ticker.info['currency']
-pretty_price = f"{ticker_name} current price is {ticker_price} {ticker_currency}"
+class Ticker:
+    def __init__(self):
+        self.ticker_name = input_ticker()
+        self.ticker = yf.Ticker(self.ticker_name)
 
-pp.pprint(pretty_price)
+    def get_info(self):
+        if self.ticker.info['logo_url'] == '':
+            raise Exception('Such ticker does not exist')
+        return self.ticker.info
+
+    def get_current_price(self):
+        ticker_price = self.ticker.info['currentPrice']
+        ticker_currency = self.ticker.info['currency']
+        pretty_price = f"{self.ticker_name} current price is {ticker_price} {ticker_currency}"
+        return pretty_price
+
+    def __str__(self):
+        return f"{self.ticker_name}"

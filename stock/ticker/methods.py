25b3b2c9856d5d9446971b941c45c6ab032709d5
da-maltsev <danil.zlatoust9999@gmail.com>
2022-11-15 10:11:07 +0500

    optimized stat

--- stock/ticker/methods.py
+++ stock/ticker/methods.py
@@ -1,52 +1,78 @@
 from .models import Ticker
 
 
-def get_current_price(ticker_name):
-    ticker = Ticker(ticker_name)
-    ticker_price = ticker.info['currentPrice']
-    pretty_price = f"{ticker.ticker_name} current price is {ticker_price} {ticker.ticker_currency}"
-    return pretty_price
+def get_current_price(ticker_name: str, ticker: Ticker = None) -> str:
+    ticker = is_ticker(ticker_name, ticker)
+    key = 'currentPrice'
+    if key in ticker.info.keys():
+        ticker_price = ticker.info[key]
+        if ticker_price:
+            return f"{ticker.ticker_name} current price is {ticker_price} {ticker.ticker_currency}"
+    return f'There is no current price for {ticker.ticker_name}'
 
 
-def get_day_low(ticker_name):
-    ticker = Ticker(ticker_name)
-    day_low = ticker.info['dayLow']
-    pretty_day_low = f"{ticker.ticker_name} day low is {day_low} {ticker.ticker_currency}"
-    return pretty_day_low
+def get_day_low(ticker_name: str, ticker: Ticker = None) -> str:
+    ticker = is_ticker(ticker_name, ticker)
+    key = 'dayLow'
+    if key in ticker.info.keys():
+        day_low = ticker.info[key]
+        if day_low:
+            return f"{ticker.ticker_name} day low is {day_low} {ticker.ticker_currency}"
+    return f'There is no day low for {ticker.ticker_name}'
 
 
-def get_day_high(ticker_name):
-    ticker = Ticker(ticker_name)
-    day_high = ticker.info['dayHigh']
-    pretty_day_high = f"{ticker.ticker_name} day high is {day_high} {ticker.ticker_currency}"
-    return pretty_day_high
+def get_day_high(ticker_name: str, ticker: Ticker = None) -> str:
+    ticker = is_ticker(ticker_name, ticker)
+    key = 'dayHigh'
+    if key in ticker.info.keys():
+        day_high = ticker.info[key]
+        if day_high:
+            return f"{ticker.ticker_name} day high is {day_high} {ticker.ticker_currency}"
+    return f'There is no day high for {ticker.ticker_name}'
 
 
-def get_premarket_price(ticker_name):
-    ticker = Ticker(ticker_name)
-    price = ticker.info['preMarketPrice']
-    pretty_price = f"{ticker.ticker_name} preMarket price is {price} {ticker.ticker_currency}"
-    return pretty_price
+def get_premarket_price(ticker_name: str, ticker: Ticker = None) -> str:
+    ticker = is_ticker(ticker_name, ticker)
+    key = 'preMarketPrice'
+    if key in ticker.info.keys():
+        price = ticker.info[key]
+        if price:
+            return f"{ticker.ticker_name} preMarket price is {price} {ticker.ticker_currency}"
+    return f'There is no preMarket price for {ticker.ticker_name}'
 
 
-def get_regular_market_price(ticker_name):
-    ticker = Ticker(ticker_name)
-    price = ticker.info['dayHigh']
-    pretty_price = f"{ticker.ticker_name} regular market price is {price} {ticker.ticker_currency}"
-    return pretty_price
+def get_regular_market_price(ticker_name: str, ticker: Ticker = None) -> str:
+    ticker = is_ticker(ticker_name, ticker)
+    key = 'regularMarketPrice'
+    if key in ticker.info.keys():
+        price = ticker.info[key]
+        if price:
+            return f"{ticker.ticker_name} regular market price is {price} {ticker.ticker_currency}"
+    return f'There is no regular market price for {ticker.ticker_name}'
 
-def get_stat(ticker_name):
-    ticker = Ticker(ticker_name)
+
+def get_stat(ticker_name: str, ticker: Ticker = None) -> str:
+    ticker = is_ticker(ticker_name, ticker)
     stats = [
-        get_current_price(ticker_name),
-        get_regular_market_price(ticker_name),
-        get_premarket_price(ticker_name),
-        get_day_low(ticker_name),
-        get_day_high(ticker_name)
+        get_current_price(ticker_name, ticker),
+        get_regular_market_price(ticker_name, ticker),
+        get_premarket_price(ticker_name, ticker),
+        get_day_low(ticker_name, ticker),
+        get_day_high(ticker_name, ticker),
     ]
     ticker_len = len(ticker.ticker_name)
     pretty_stat = f'{ticker.ticker_name}:\n'
     for stat in stats:
-        pretty_stat += f' -{stat[ticker_len:]}\n'
+        if 'There is no' in stat:
+            pretty_stat += f' - {stat}\n'
+        else:
+            pretty_stat += f' -{stat[ticker_len:]}\n'
 
     return pretty_stat
+
+
+def is_ticker(ticker_name: str, ticker: Ticker) -> Ticker:
+    if not ticker:
+        return Ticker(ticker_name)
+    else:
+        return ticker
